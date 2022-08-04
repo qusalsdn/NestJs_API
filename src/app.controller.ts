da90047dbec13f7a3a665d988a93523f8f2ace79
MinWooByun <yja05092@gmail.com>
2022-08-04 15:55:58 +0900

    Controllers
    컨트롤러 연습
    컨드롤러는 express의 미들웨어와 같다. url를 넘겨주고 함수나 클래스를 매핑하여 실행해준다.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 컨드롤러는 express의 미들웨어와 같다. url를 넘겨주고 함수나 클래스를 매핑하여 실행해준다.
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -8,5 +10,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello')
+  getHi(): string {
+    return this.appService.getHi();
   }
 }
