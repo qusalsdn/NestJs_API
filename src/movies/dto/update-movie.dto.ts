ba375f4098c52d79e72b5c6a7f5009057980fcaf
MinWooByun <yja05092@gmail.com>
2022-08-09 18:15:42 +0900

    Module 분리
    1. 기본 app에서 movies module로 분리
    2. Dependency Injection은 모듈에서 컨트롤러 및 서비스를 이용할 수 있도록 NestJs가 알아서 import 해주는 것을 말한다.

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,7 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsNumber, IsString } from 'class-validator';
 import { CreateMovieDTO } from './create-movie.dto';
-
-// ?를 붙혀 필수가 아니라고 설정해준다.
 
 export class UpdateMovieDTO extends PartialType(CreateMovieDTO) {}
