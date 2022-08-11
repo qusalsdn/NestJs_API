9fc5f10c96f32546d1f0de06d4613d5272d52dab
MinWooByun <yja05092@gmail.com>
2022-08-11 17:59:58 +0900

    Testing PATCH and DELETE movies id
    1. movies를 생성할 때 잘못된 정보를 기입 했을 때 에러가 잡히는지 테스트
    2. PATCH와 DELETE를 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -35,7 +35,7 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
 
-    it('POST', () => {
+    it('POST 201', () => {
       return (
         request(app.getHttpServer())
           .post('/movies')
@@ -43,6 +43,13 @@ describe('AppController (e2e)', () => {
           // 201은 생성되었다는 뜻이다.
           .expect(201)
       );
+    });
+
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({ title: 'Test', year: 2000, genres: ['test'], other: 'thing' })
+        .expect(400);
     });
 
     it('DELETE', () => {
@@ -53,11 +60,21 @@ describe('AppController (e2e)', () => {
       it('GET 200', () => {
         return request(app.getHttpServer()).get('/movies/1').expect(200);
       });
+
       it('GET 404', () => {
         return request(app.getHttpServer()).get('/movies/999').expect(404);
       });
-      it.todo('DELETE');
-      it.todo('PATCH');
+
+      it('PATCH 200', () => {
+        return request(app.getHttpServer())
+          .patch('/movies/1')
+          .send({ title: 'Updated Test' })
+          .expect(200);
+      });
+
+      it('DELETE 200', () => {
+        return request(app.getHttpServer()).delete('/movies/1').expect(200);
+      });
     });
   });
 });
